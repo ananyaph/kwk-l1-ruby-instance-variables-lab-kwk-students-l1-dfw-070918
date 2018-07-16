5822f2154ed1883c221f5fef2de74d4c73fb86de
Ananya Phadke <phadkeananya@gmail.com>
2018-07-16 16:43:14 +0000

    automatically backed up by learn

---
+++ songs.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+name = gets.chomp
+puts "Happy Birthday to you!"
+puts "Happy Birthday to you!"
+puts "Happy Birthday dear #{name}!"
+puts "Happy Birthday to you!" 
+
+number = 99
+loop do
+puts "#{number} bottles of Root Beer on the wall, 99 bottles of Root Beer. "
+
+number -= 1
+
+puts "Take one down and pass it around, #{number} of bottles of Root Beer on the wall."
+
+end
